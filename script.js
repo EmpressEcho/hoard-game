892086269168aa5bf583324d940dfc250c94aa90
Echo De Winter <heyitsecho@protonmail.com>
2023-08-09 17:37:15 +0100

    created layout + linked to existing JS

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const magicButton = document.querySelector(".magic")
-const progBar = document.querySelector(".progbar")
+const magicButton = document.getElementById("quest1")
+const progBar1 = document.getElementById("progBar1")
+const goldTracker = document.getElementById("gold")
+
+function updateGold (earnedGold) {
+    let gold = Number(goldTracker.textContent)
+    gold += earnedGold
+    goldTracker.textContent = gold
+}
 
 magicButton.addEventListener("click", function() {
-    progBar.animate([
+    progBar1.animate([
         {
             width: "0%"
         },
         {
             width: "100%"
         }
-    ], 2000)
+    ], 2000);
+    setTimeout(() => updateGold(2), 2000);
 })
