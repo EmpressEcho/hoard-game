c8b0359f389002c34df988de430340de3e4a629f
Echo De Winter <heyitsecho@protonmail.com>
2023-08-10 16:45:59 +0100

    Implemented full hidden layout + proof of JS for displaying

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const magicButton = document.getElementById("quest1")
 const progBar1 = document.getElementById("progBar1")
 const goldTracker = document.getElementById("gold")
 
+const quest2 = document.getElementById("quest2")
+
 function updateGold (earnedGold) {
     let gold = Number(goldTracker.textContent)
     gold += earnedGold
     goldTracker.textContent = gold
+    updateQuests()
+}
+
+function updateQuests () {
+    let gold = Number(goldTracker.textContent)
+    if (gold >= 10) (quest2.classList.toggle("hidden"))
 }
 
 magicButton.addEventListener("click", function() {
@@ -17,5 +25,5 @@ magicButton.addEventListener("click", function() {
             width: "100%"
         }
     ], 2000);
-    setTimeout(() => updateGold(2), 2000);
+    setTimeout(() => updateGold(1), 2000);
 })
