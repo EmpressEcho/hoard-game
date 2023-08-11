0ed7bdd2322fd98ea14cc06fb77789b73b1eb932
Echo De Winter <heyitsecho@protonmail.com>
2023-08-11 17:04:44 +0100

    refactored variables

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
-const magicButton = document.getElementById("quest1")
-const progBar1 = document.getElementById("progBar1")
 const goldTracker = document.getElementById("gold")
 
+// --- Quest Elements ---
+const quest1 = document.getElementById("quest1")
 const quest2 = document.getElementById("quest2")
+const quest3 = document.getElementById("quest3")
+const quest4 = document.getElementById("quest4")
+const quest5 = document.getElementById("quest5")
+const quest6 = document.getElementById("quest6")
+const quest7 = document.getElementById("quest7")
+const quest8 = document.getElementById("quest8")
+const quest9 = document.getElementById("quest9")
+const quest10 = document.getElementById("quest10")
 
+// --- Quest Progress Bars ---
+const progBarQ1 = document.getElementById("progBar1")
+const progBarQ2 = document.getElementById("progBar2")
+const progBarQ3 = document.getElementById("progBar3")
+const progBarQ4 = document.getElementById("progBar4")
+const progBarQ5 = document.getElementById("progBar5")
+const progBarQ6 = document.getElementById("progBar6")
+const progBarQ7 = document.getElementById("progBar7")
+const progBarQ8 = document.getElementById("progBar8")
+const progBarQ9 = document.getElementById("progBar9")
+const progBarQ10 = document.getElementById("progBar10")
+
+// --- Minion Elements ---
+const minionKobolds = document.getElementById("minionKobolds")
+const minionBandits = document.getElementById("minionBandits")
+const minionCultists = document.getElementById("minionCultists")
+const minionDragonborn = document.getElementById("minionDragonborn")
+const minionElementals = document.getElementById("minionElementals")
+
+// --- Minion Progress Bars ---
+const progBarKobolds = document.getElementById("progBarM1")
+const progBarBandits = document.getElementById("progBarM2")
+const progBarCultists = document.getElementById("progBarM3")
+const progBarDragonborn = document.getElementById("progBarM4")
+const progBarElementals = document.getElementById("progBarM5")
+
+// --- Store Elements ---
 const storeButton = document.querySelector(".storeButton")
 const store = document.querySelector(".store")
 
+// --- Store Minions ---
+const storeKobolds = document.getElementById("storeKobolds")
+const storeBandits = document.getElementById("storeBandits")
+const storeCultists = document.getElementById("storeCultists")
+const storeDragonborn = document.getElementById("storeDragonborn")
+const storeElementals = document.getElementById("storeElementals")
+
+// --- User Profile Elements ---
 const userProfileButton = document.querySelector(".userProfile")
 const userProfileOptions = document.querySelector(".userProfileOptions")
+const userLogin = document.getElementById("userLogin")
+const userPrefs = document.getElementById("userPrefs")
+const userAchievements = document.getElementById("userAchievements")
+const userLogout = document.getElementById("userLogout")
+
+// --------------
 
 function updateGold (earnedGold) {
     let gold = Number(goldTracker.textContent)
