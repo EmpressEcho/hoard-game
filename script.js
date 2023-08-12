a33d9e2cd12b18b110788ccaa86adc7ac7fe9f8a
Echo De Winter <heyitsecho@protonmail.com>
2023-08-12 14:59:08 +0100

    added gold functionality to all quests and minions

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const goldTracker = document.getElementById("gold");
 
 // --- Quest Elements ---
-const quest1 = document.getElementById("quest1");
-const quest2 = document.getElementById("quest2");
-const quest3 = document.getElementById("quest3");
-const quest4 = document.getElementById("quest4");
-const quest5 = document.getElementById("quest5");
-const quest6 = document.getElementById("quest6");
-const quest7 = document.getElementById("quest7");
-const quest8 = document.getElementById("quest8");
-const quest9 = document.getElementById("quest9");
-const quest10 = document.getElementById("quest10");
+const quest1Element = document.getElementById("quest1");
+const quest2Element = document.getElementById("quest2");
+const quest3Element = document.getElementById("quest3");
+const quest4Element = document.getElementById("quest4");
+const quest5Element = document.getElementById("quest5");
+const quest6Element = document.getElementById("quest6");
+const quest7Element = document.getElementById("quest7");
+const quest8Element = document.getElementById("quest8");
+const quest9Element = document.getElementById("quest9");
+const quest10Element = document.getElementById("quest10");
 
 // --- Quest Progress Bars ---
 const progBarQ1 = document.getElementById("progBar1");
@@ -24,42 +24,105 @@ const progBarQ8 = document.getElementById("progBar8");
 const progBarQ9 = document.getElementById("progBar9");
 const progBarQ10 = document.getElementById("progBar10");
 
+// --- Quest Variables ---
+const quest1 = {
+    element: quest1Element,
+    progBar: progBarQ1,
+    reward: 1,
+    delay: 1000
+};
+const quest2 = {
+    element: quest2Element,
+    progBar: progBarQ2,
+    reward: 1,
+    delay: 1000
+};
+const quest3 = {
+    element: quest3Element,
+    progBar: progBarQ3,
+    reward: 1,
+    delay: 1000
+};
+const quest4 = {
+    element: quest4Element,
+    progBar: progBarQ4,
+    reward: 1,
+    delay: 1000
+};
+const quest5 = {
+    element: quest5Element,
+    progBar: progBarQ5,
+    reward: 1,
+    delay: 1000
+};
+const quest6 = {
+    element: quest6Element,
+    progBar: progBarQ6,
+    reward: 1,
+    delay: 1000
+};
+const quest7 = {
+    element: quest7Element,
+    progBar: progBarQ7,
+    reward: 1,
+    delay: 1000
+};
+const quest8 = {
+    element: quest8Element,
+    progBar: progBarQ8,
+    reward: 1,
+    delay: 1000
+};
+const quest9 = {
+    element: quest9Element,
+    progBar: progBarQ9,
+    reward: 1,
+    delay: 1000
+};
+const quest10 = {
+    element: quest10Element,
+    progBar: progBarQ10,
+    reward: 1,
+    delay: 1000
+};
+const questArray = [quest1, quest2, quest3, quest4, quest5, quest6, quest7, quest8, quest9, quest10];
+
 // --- Quest Unlock Levels ---
 const questUnlockArray = [
     {
-        questElement: quest2,
+        questElement: quest2Element,
         unlockThreshold: 10
     },
     {
-        questElement: quest3,
+        questElement: quest3Element,
         unlockThreshold: 20
     },
     {
-        questElement: quest4,
+        questElement: quest4Element,
         unlockThreshold: 30
     },
     {
-        questElement: quest5,
+        questElement: quest5Element,
         unlockThreshold: 40
     },
     {
-        questElement: quest6,
+        questElement: quest6Element,
         unlockThreshold: 50
     },
     {
-        questElement: quest7,
+        questElement: quest7Element,
         unlockThreshold: 60
     },
     {
-        questElement: quest8,
+        questElement: quest8Element,
         unlockThreshold: 70
     },
     {
-        questElement: quest9,
+        questElement: quest9Element,
         unlockThreshold: 80
     },
     {
-        questElement: quest10,
+        questElement: quest10Element,
         unlockThreshold: 90
     },
 ];
@@ -89,49 +152,65 @@ const storeCultists = document.getElementById("storeCultists");
 const storeDragonborn = document.getElementById("storeDragonborn");
 const storeElementals = document.getElementById("storeElementals");
 
-// --- Minion Variables ---
-let kobolds = {
-    number: 0,
-    delay: 2000,
-    value: 3,
-    cost: 10,
-    progBar: progBarKobolds
-};
-let bandits = {
-    number: 0,
-    delay: 2000,
-    value: 3,
-    cost: 10,
-    progBar: progBarBandits
-};
-let cultists = {
-    number: 0,
-    delay: 2000,
-    value: 3,
-    cost: 10,
-    progBar: progBarCultists
-};
-let dragonborn = {
-    number: 0,
-    delay: 2000,
-    value: 3,
-    cost: 10,
-    progBar: progBarDragonborn
-};
-let elementals = {
-    number: 0,
-    delay: 2000,
-    value: 3,
-    cost: 10,
-    progBar: progBarElementals
-};
-
 // --- Minion Intervals ---
 let koboldInterval = null;
 let banditInterval = null;
 let cultistInterval = null;
 let dragonbornInterval = null;
 let elementalInterval = null;
+
+// --- Minion Variables ---
+let kobolds = {
+    number: 0,
+    delay: 2000,
+    value: 3,
+    cost: 10,
+    progBar: progBarKobolds,
+    storeElement: storeKobolds,
+    element: minionKobolds,
+    interval: koboldInterval
+};
+let bandits = {
+    number: 0,
+    delay: 2000,
+    value: 3,
+    cost: 10,
+    progBar: progBarBandits,
+    storeElement: storeBandits,
+    element: minionBandits,
+    interval: banditInterval
+};
+let cultists = {
+    number: 0,
+    delay: 2000,
+    value: 3,
+    cost: 10,
+    progBar: progBarCultists,
+    storeElement: storeCultists,
+    element: minionCultists,
+    interval: cultistInterval
+};
+let dragonborn = {
+    number: 0,
+    delay: 2000,
+    value: 3,
+    cost: 10,
+    progBar: progBarDragonborn,
+    storeElement: storeDragonborn,
+    element: minionDragonborn,
+    interval: dragonbornInterval
+};
+let elementals = {
+    number: 0,
+    delay: 2000,
+    value: 3,
+    cost: 10,
+    progBar: progBarElementals,
+    storeElement: storeElementals,
+    element: minionElementals,
+    interval: elementalInterval
+};
+let minionArray = [kobolds, bandits, cultists, dragonborn, elementals];
 
 // --- User Profile Elements ---
 const userProfileButton = document.querySelector(".userProfile");
@@ -147,18 +226,18 @@ function updateGold (earnedGold) {
     let gold = Number(goldTracker.textContent);
     gold += earnedGold;
     goldTracker.textContent = gold;
-    if (earnedGold > 0) (updateQuests());
+    updateQuests();
 };
 
 function updateQuests () {
     let gold = Number(goldTracker.textContent);
-    for(let i = 0; i <= questUnlockArray.length; i++) {
+    for(let i = 0; i < questUnlockArray.length; i++) {
         if (gold >= questUnlockArray[i].unlockThreshold && questUnlockArray[i].questElement.classList.contains("hidden")) (questUnlockArray[i].questElement.classList.remove("hidden"));
     }
 };
 
 function updateMinionInterval (minionInterval, minion) {
-    if (minionInterval !== null) (clearInterval(minionInterval));
+    clearInterval(minionInterval);
     minionInterval = setInterval(runMinion, minion.delay, minion);
 }
 
@@ -183,31 +262,35 @@ function toggleHidden (element) {
     element.classList.toggle("hidden");
 };
 
-quest1.addEventListener("click", function() {
-    progBar1.animate([
-        {
-            width: "0%"
-        },
-        {
-            width: "100%"
-        }
-    ], 1000);
-    setTimeout(() => updateGold(10), 1000);
-})
+for (let i = 0; i < questArray.length; i++) {
+    questArray[i].element.addEventListener("click", function() {
+        questArray[i].progBar.animate([
+            {
+                width: "0%"
+            },
+            {
+                width: "100%"
+            }
+        ], questArray[i].delay);
+        setTimeout(() => updateGold(questArray[i].reward), questArray[i].delay);
+    })
+}
 
 storeButton.addEventListener("click", function() {
     toggleHidden(store);
     storeButton.textContent === "Visit the Store" ? storeButton.textContent = "Close the Store" : storeButton.textContent = "Visit the Store";
 })
 
-storeKobolds.addEventListener("click", function() {
-    if (Number(goldTracker.textContent) >= kobolds.cost) {
-        updateGold(0-kobolds.cost);
-        kobolds.number++
-        updateMinionElement(minionKobolds);
-        updateMinionInterval(koboldInterval, kobolds);
-    }
-})
+for (let i = 0; i < minionArray.length; i++) {
+    minionArray[i].storeElement.addEventListener("click", function() {
+        if (Number(goldTracker.textContent) >= minionArray[i].cost) {
+            updateGold(0-minionArray[i].cost);
+            minionArray[i].number++
+            updateMinionElement(minionArray[i].element);
+            updateMinionInterval(minionArray[i].interval, minionArray[i]);
+        }
+    })
+}
 
 userProfileButton.addEventListener("click", function() {
     toggleHidden(userProfileOptions);
