eba0f0f14946fe1eeace0773c8370a865d65f6bc
EmpressEcho <heyitsecho@protonmail.com>
2024-02-29 13:24:08 +0000

    fixed error

--- tools.py
+++ tools.py
@@ -3,7 +3,7 @@ def create_list(raw_data, expected_sort):
 
     for x in expected_sort:
       for y in raw_data:
-          if y.id == x:
+          if y["id"] == x:
               result.append(y)
 
     return result
